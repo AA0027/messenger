7cc8b86392d360b80d3adab7a69f1ab814365188
AA0027 <whckdtjd456@naver.com>
2024-08-19 09:06:30 +0900

    구둑문제 해결

---
+++ src/components/Chat/Message.jsx
@@ -0,0 +1,54 @@
+import { Avatar } from '@mui/material';
+import { deepPurple } from '@mui/material/colors';
+import React, { useEffect, useRef } from 'react';
+import './Message.css';
+const Message = (prop) => {
+    const { m, my_name} = prop;
+
+    if(m.sender.name === my_name){
+        return (
+            <div className='message my'>
+                <div className='message-info'>
+                    <div className='date'>{(m.regdate.split(" "))[1]}</div>
+                    <div className='msg-bowl'>
+                        {m.content && <div className='msg'>{m.content}</div>}
+                        {m.files && m.files.map((f) => <div className='file-msg'>{f.sourcename}</div>)}
+                    </div> 
+                    <Avatar sx={{ bgcolor: deepPurple[500], width: "40px"
+                        , height: "40px", }}>
+                            <div className='name'>{m.sender.name}</div>
+                    </Avatar>
+                    
+                </div>
+                
+            </div>
+         );  
+    }
+    else {
+        return (
+            <div className='message you' >
+                <div>
+                    <div className='avartar'>
+                        <Avatar sx={{ bgcolor: deepPurple[500], width: "40px"
+                            , height: "40px"}} >
+                                <div className='name'>{m.sender.name}</div>
+                        </Avatar>
+                    </div>
+                    <div className='msg-bowl'>
+                        {m.content !== "" && <div>{m.content}</div>}
+                        {m.files && m.files.map((f) => <div className='file-msg'>{f.sourcename}</div>)}
+                    </div> 
+                    <div className='date'>
+                        {
+                            (m.regdate.split(" "))[1]
+                        }
+                    </div>
+                </div>
+                
+            </div>
+         );
+    }
+    
+};
+
+export default Message;
