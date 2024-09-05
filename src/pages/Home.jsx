9ac74f8f70656fa9f003b1031bdd5083e85d0222
AA0027 <whckdtjd456@naver.com>
2024-09-05 13:14:19 +0900

    messenger

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,43 +1,43 @@
 import React, { useContext, useEffect, useState } from 'react';
+import Cookies from 'js-cookie';
 import Sidebar from '../components/Sidebar/Sidebar';
-import Channel from '../components/Channel/Channel';
-import '../components/Channel/Channel.css';
-import * as data from '../apis/data';
 import { LoginContext } from '../contexts/LoginContextProvider';
-const MyChannel = () => {
+import Channel from '../components/Channel/Channel';
+import * as data from '../apis/data';
+import { HOST } from '../apis/api';
+import { Stomp } from '@stomp/stompjs';
+const Home = () => {
     const [ channelList, setChannelList ] = useState([]);
-    const { userInfo, isLogin, inviteCard} = useContext(LoginContext);
+    const [ inviteF, setInviteF ] = useState(true);
+    const { userInfo, isLogin, inviteCard, getInvite } = useContext(LoginContext);
     useEffect(  ()=>{
         getChannelList();
-        console.log("초대장 변경");
-        console.log(inviteCard);
-    }, [inviteCard]);
+        
+        getInvite({code: "", username: userInfo.username});
+    }, [inviteF]);
+  
 
     const getChannelList = async () => {
-        console.log(inviteCard);
         const response = await data.channelList(userInfo.username);
         setChannelList([...(response.data)]);
     }
+
     return (
         <>
             {
                 isLogin &&
                 <div className="container">
-                    <Sidebar/>
-                
+                    <Sidebar inviteF={inviteF} setInviteF={setInviteF}/>
                     <div className=' content jua-regular'>
                         <div className='head'>My Channel</div>
                         <div className='channel-list'>
-                            {channelList && channelList.map(e => (<Channel key={e.id} code={e.code} name={e.name}
-                            creator={e.creator.username} channelList={channelList} setChannelList={setChannelList}/>)) }
+                            {channelList && channelList.map(e => (<Channel key={e.id} name={e.name} code={e.code} channelList={channelList} setChannelList={setChannelList}/>)) }
                         </div>
                     </div>
-                
                 </div>
             }
         </>
-        
     );
 };
 
-export default MyChannel;
+export default Home;
